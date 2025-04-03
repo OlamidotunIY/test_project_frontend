694624ff5bee7a1d6dd7db780fc1c1b51b6244b0
Iyanda Olamidotun <olamidotun225@gmail.com>
2025-04-03 14:46:06 +0100

    add: env

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,10 @@ export default defineConfig({
     host: "0.0.0.0", // Expose to all interfaces
     port: 8080, // Ensure it's using port 8080 (or any other port you want)
   },
+  preview: {
+    allowedHosts: [
+      'user-list-app-8glm.onrender.com', // Add your Render domain here
+      'localhost',  // Localhost is usually allowed by default
+    ],
+  },
 });
